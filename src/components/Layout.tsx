b66b2e30d2821306a0f400aba3c12566a1f378b3
Facundo <fgiacconi@uoc.edu>
2023-04-25 14:07:25 +0200

    Initial commit from Create Next App

---
+++ src/components/Layout.tsx
@@ -0,0 +1,36 @@
+import { FC, ReactNode } from 'react';
+import Head from 'next/head';
+import { Navbar } from '@nextui-org/react';
+
+interface Props {
+  title?: string;
+  children?: ReactNode;
+}
+
+
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+
+
+export const Layout: FC<Props> = ({ title, children }) => {
+
+
+  return (
+      <>
+        <Head>
+            <title>{ 'Precio de la luz' }</title>
+            <meta name="author" content="Facundo Giacconi" />
+            <meta name="description" content={`Información sobre el precio de la luz de hoy dia ${title}`} />
+            <meta property="og:title" content={`Información sobre el precio de la luz (€/Mwh)`} />
+        </Head>
+      
+        <Navbar />
+
+        <main style={{
+          padding: '0px 20px'
+        }}>
+            { children }
+        </main>
+      
+      </>
+  )
+};
